2fae459fe653d9c19a235c45098526aeea3f712a
Fatih Kablan <kablanfatih34@gmail.com>
2020-06-16 21:34:26 +0300

    entities created

---
+++ src/main/java/com/kablanfatih/issuemanagement/entity/User.java
@@ -0,0 +1,36 @@
+package com.kablanfatih.issuemanagement.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "uname", length = 100, unique = true)
+    private String username;
+
+    @Column(name = "pwd", length = 100)
+    private String password;
+
+    @Column(name = "full_name", length = 100)
+    private String fullName;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @JoinColumn(name = "assignee_user_id")
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Issue> issues;
+}
