0278bc24077b7feefff5c5a46f26fa181bf5fac1
Fatih Kablan <kablanfatih34@gmail.com>
2020-06-23 00:55:12 +0300

    logging

--- src/main/java/com/kablanfatih/issuemanagement/exception/IMExceptionHandler.java
+++ src/main/java/com/kablanfatih/issuemanagement/exception/IMExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.kablanfatih.issuemanagement.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,14 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 import java.util.Date;
 
 @ControllerAdvice
 @RestController
+@Slf4j
 public class IMExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<?> handleExceptions(Exception ex, WebRequest request) {
+        log.error("ControllerAdvice -> ExceptionHandler -> ", ex, request);
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage());
         return new ResponseEntity<>(exceptionResponse, HttpStatus.EXPECTATION_FAILED);
     }
